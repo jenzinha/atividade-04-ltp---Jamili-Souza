639a630edf5f94b471d440a0d39e7390e02dfd05
jenniekkj <101286269+jenzinha@users.noreply.github.com>
2022-03-09 18:50:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Retangulo {
+  constructor(altura, largura){
+    this.altura = altura;  
+    this.largura = largura;
+  }
+  calcularArea(){
+     return this.largura * this.altura 
+  }
+  area(){
+    return this.calcularArea();
+  }
+}
+
+let primeiroObjeto = new Retangulo("10", "10");
+console.log(primeiroObjeto.area())
+
+let segundoObjeto = new Retangulo("60", "30");
+console.log(segundoObjeto.area())
